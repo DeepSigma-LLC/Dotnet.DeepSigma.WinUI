a1056f4eb61849be82dea0489d30680dcb3fd1d0
Brenden Perry <brendenperry5@gmail.com>
2026-07-10 13:50:15 -0700

    Use record for ControlSelectionItem; add implicit
    Replace the generic ControlSelectionItem class with a record class (positional properties and updated XML docs). Add an implicit conversion from T to ObservableProperty<T> to simplify wrapping values. Bump DeepSigma.WinUI package version to 1.9.0.0.

--- DeepSigma.WinUI/ControlSelectionItem.cs
+++ DeepSigma.WinUI/ControlSelectionItem.cs
@@ -7,27 +7,7 @@ namespace DeepSigma.WinUI;
 /// <summary>
 /// Represents an item in a control selection, with a display name and a value of type T.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-public class ControlSelectionItem<T> where T : notnull
-{
-    /// <summary>
-    /// Gets or sets the display name of the item.
-    /// </summary>
-    public required string DisplayName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the value associated with the item.
-    /// </summary>
-    public required T Value { get; set; }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ControlSelectionItem{T}"/> class with the specified display name and value.
-    /// </summary>
-    /// <param name="displayName"></param>
-    /// <param name="value"></param>
-    public ControlSelectionItem(string displayName, T value)
-    {
-        DisplayName = displayName;
-        Value = value;
-    }
-}
+/// <typeparam name="T">The type of the value associated with the item.</typeparam>
+/// <param name="DisplayName">The display name of the item.</param>
+/// <param name="Value">The value associated with the item.</param>
+public record class ControlSelectionItem<T>(string DisplayName, T Value) where T : notnull;
--- DeepSigma.WinUI/ObservableProperty.cs
+++ DeepSigma.WinUI/ObservableProperty.cs
@@ -72,4 +72,10 @@ public partial class ObservableProperty<T> : INotifyPropertyChanged where T : no
     /// </summary>
     /// <param name="wrapper">The wrapper instance to convert.</param>
     public static implicit operator T(ObservableProperty<T> wrapper) => wrapper.Value;
+
+    /// <summary>
+    /// Defines an implicit conversion from T to PropertyChangeNotificationWrapper<T>, allowing a value of type T to be automatically wrapped in a PropertyChangeNotificationWrapper<T> instance.
+    /// </summary>
+    /// <param name="value">The value to wrap.</param>
+    public static implicit operator ObservableProperty<T>(T value) => new(value);
 }
